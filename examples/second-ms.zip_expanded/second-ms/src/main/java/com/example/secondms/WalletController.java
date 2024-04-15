f8f59f8c1b454ae78cd14373b11873b951f79079
Kishor <kishor1124@gmail.com>
2024-04-15 22:19:38 +0530

    spring cloud configuration

--- examples/second-ms.zip_expanded/second-ms/src/main/java/com/example/secondms/WalletController.java
+++ examples/second-ms.zip_expanded/second-ms/src/main/java/com/example/secondms/WalletController.java
@@ -1,6 +1,10 @@
 package com.example.secondms;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +17,20 @@ public class WalletController {
 
 	@Autowired
 	private WalletService service;
+	
+	@Value("${database.username}")
+	private String username;
+	
+	@Value("${database.password}")
+	private String password;
+	
+	@GetMapping("/config")
+	public ResponseEntity<Object> getProperties() {
+		Map<String, String> configMap = new LinkedHashMap<>();
+		configMap.put("username", username);
+		configMap.put("password", password);
+		return ResponseEntity.status(200).body(configMap);
+	}
 	
 	@GetMapping("/{accountNumber}")
 	public ResponseEntity<Object> fetchWalletDetails(@PathVariable long accountNumber) {
